a2d11ba4103603603c8f16b38eaf268bc690e0cc
hj2222222 <111670814+hj2222222@users.noreply.github.com>
2023-05-07 03:33:02 +0900

    1차
    1차적으로 코드 복제 및 모델 짤 준비

---
+++ util.py
@@ -0,0 +1,44 @@
+import os
+import cv2
+import torch
+
+CLASS_NAME_TO_ID = {'Bus': 0, 'Truck': 1}
+CLASS_ID_TO_NAME = {0: 'Bus', 1: 'Truck'}
+BOX_COLOR = {'Bus':(200, 0, 0), 'Truck':(0, 0, 200)}
+
+TEXT_COLOR = (255, 255, 255)
+
+def save_model(model_state, model_name, save_dir="./trained_model"):
+    os.makedirs(save_dir, exist_ok=True)
+    torch.save(model_state, os.path.join(save_dir, model_name))
+
+
+def visualize_bbox(image, bbox, class_name, color=BOX_COLOR, thickness=2):
+    x_center, y_center, w, h = bbox
+    x_min = int(x_center - w/2)
+    y_min = int(y_center - h/2)
+    x_max = int(x_center + w/2)
+    y_max = int(y_center + h/2)
+    
+    cv2.rectangle(image, (x_min, y_min), (x_max, y_max), color=color[class_name], thickness=thickness)
+    
+    ((text_width, text_height), _) = cv2.getTextSize(class_name, cv2.FONT_HERSHEY_SIMPLEX, 0.35, 1)    
+    cv2.rectangle(image, (x_min, y_min - int(1.3 * text_height)), (x_min + text_width, y_min), color[class_name], -1)
+    cv2.putText(
+        image,
+        text=class_name,
+        org=(x_min, y_min - int(0.3 * text_height)),
+        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+        fontScale=0.35, 
+        color=TEXT_COLOR, 
+        lineType=cv2.LINE_AA,
+    )
+    return image
+
+
+def visualize(image, bboxes, category_ids):
+    img = image.copy()
+    for bbox, category_id in zip(bboxes, category_ids):
+        class_name = CLASS_ID_TO_NAME[category_id]
+        img = visualize_bbox(img, bbox, class_name)
+    return img
